17d2af8e8ea2f5b3120cd81702008db6075837ec
JayLee1010 <73064612+JayLee1010@users.noreply.github.com>
2020-10-21 22:44:49 +0900

    Day1_Server1

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log("Hi");
-a;
+const express = require("express");
+const app = express();
+const port = 4000;
+
+function handlelistening() {
+  console.log(`listening on: http://localhost:${port}`);
+}
+
+app.listen(port, handlelistening);
+
+/*
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+*/
