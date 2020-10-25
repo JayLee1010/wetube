197119e700c7e1ecac1cc56b4e4e6837f4cbe06e
JayLee1010 <73064612+JayLee1010@users.noreply.github.com>
2020-10-25 22:08:45 +0900

    Day3-1 MVC part1

--- app.js
+++ app.js
@@ -3,24 +3,20 @@ import logger from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
-const handlehome = (req, res) => res.send("Hi from home");
-
-const handleprofile = (req, res) => res.send("This is my profile");
-
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(logger("dev"));
 
-app.get("/", handlehome);
-
-app.get("/profile", handleprofile);
-
-app.use("/user", userRouter);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 export default app;
